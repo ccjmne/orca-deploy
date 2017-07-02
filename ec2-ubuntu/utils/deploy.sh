ed208e642f37f87c7de2cf5f42703ac550ec347c
ccjmne <ccjmne@gmail.com>
2017-07-02 19:05:28 +0200

    No need to run as root or with sudo anymore. Just need to be in the sudoers.

--- ec2-ubuntu/utils/deploy.sh
+++ ec2-ubuntu/utils/deploy.sh
@@ -3,6 +3,6 @@ set -e
 
 mkdir __tmp && cd __tmp
 wget `curl -s https://api.github.com/repos/ccjmne/orca-deploy/releases | grep browser_download_url | grep setup.tar.gz | head -n 1 | cut -d '"' -f 4` && tar -zxvf setup.tar.gz
-sed -i *.sh -e 's/\r$//' && chmod +x *.sh && ./setup.sh
-mv orca.conf .. && mv update.sh ..
+sed -i *.{sh,conf} -e 's/\r$//' && chmod +x *.sh && sudo ./setup.sh
+mv -t ~ orca.conf update.sh
 cd - && rm -Rf __tmp
