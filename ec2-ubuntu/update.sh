336514155b6cd4456ab83991d594213b77ff689a
ccjmne <ccjmne@gmail.com>
2018-02-07 19:25:35 +0100

    Docker memory limit removed, restart policy updated (unless-stopped)

--- ec2-ubuntu/update.sh
+++ ec2-ubuntu/update.sh
@@ -23,7 +23,7 @@ ${__NF} Updating Orca to \033[1;97morca:${TAG}\033[0m\n\
 sudo `aws ecr get-login --no-include-email --region eu-west-1`
 sudo docker pull 424880512736.dkr.ecr.eu-west-1.amazonaws.com/orca:$TAG
 sudo docker stop orca || true && sudo docker rm orca || true
-sudo docker run -it -d --memory=420m --restart=on-failure:2 -p=8080:8080 --name=orca --env-file orca.conf 424880512736.dkr.ecr.eu-west-1.amazonaws.com/orca:$TAG
+sudo docker run -it -d --restart=unless-stopped -p=8080:8080 --name=orca --env-file orca.conf 424880512736.dkr.ecr.eu-west-1.amazonaws.com/orca:$TAG
 
 printf "===============================================================================\n\
 ${__OK} All done. Servers are up and running.\n\
