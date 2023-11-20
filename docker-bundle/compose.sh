3d1bc748c6d82d051950355a595b31a3b1c9e940
Eric NICOLAS (ccjmne) <ccjmne@gmail.com>
2023-11-20 23:05:26 +0100

    Build Orca for both AMD and ARM

--- docker-bundle/compose.sh
+++ docker-bundle/compose.sh
@@ -6,8 +6,11 @@ if [ -z "$1" ]; then
 fi
 
 echo "Make sure to have the web app available under the webapps/ directory."
-read -p "Press Enter to continue..."
+read -rp "Press Enter to continue..."
 
-docker build -t orca:"$1" -t 424880512736.dkr.ecr.eu-west-1.amazonaws.com/orca:"$1" .
-docker push 424880512736.dkr.ecr.eu-west-1.amazonaws.com/orca:"$1"
+ecr=424880512736.dkr.ecr.eu-west-1.amazonaws.com
+
+aws ecr get-login-password | docker login --username AWS --password-stdin $ecr
+docker buildx use multiarch || docker buildx create --use --name multiarch --platform amd64,arm64
+docker buildx build --platform amd64,arm64 --tag $ecr/orca:"$1" --push .
 echo "Done."
