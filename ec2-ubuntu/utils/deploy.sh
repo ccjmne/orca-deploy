3d9e650386c83992a00bf25f9ecff1da0c3796de
Eric NICOLAS (ccjmne) <ccjmne@gmail.com>
2023-11-21 00:09:00 +0100

    Expect all files to to use LF line endings

--- ec2-ubuntu/utils/deploy.sh
+++ ec2-ubuntu/utils/deploy.sh
@@ -4,6 +4,6 @@ set -e
 tmp=$(mktemp -d)
 cd "$tmp"
 wget "$(curl -s https://api.github.com/repos/ccjmne/orca-deploy/releases | grep browser_download_url | grep setup.tar.gz | head -n 1 | cut -d '"' -f 4)" && tar -zxvf setup.tar.gz
-sed -i ./*.{sh,conf} -e 's/\r$//' && chmod +x ./*.sh && sudo ./setup.sh
+chmod u+x ./*.sh && ./setup.sh
 mv -t ~ orca.conf update.sh
 cd - && rm -Rf "$tmp"
