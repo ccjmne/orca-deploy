71f2189c0994851c5106216c98d0f6c9162f42b9
Eric NICOLAS (ccjmne) <ccjmne@gmail.com>
2023-11-21 22:07:56 +0100

    Also retain utils.sh after deployment procedure
    As is it used by update.sh

--- ec2-ubuntu/utils/deploy.sh
+++ ec2-ubuntu/utils/deploy.sh
@@ -5,5 +5,5 @@ tmp=$(mktemp -d)
 cd "$tmp"
 wget "$(curl -s https://api.github.com/repos/ccjmne/orca-deploy/releases | grep browser_download_url | grep setup.tar.gz | head -n 1 | cut -d '"' -f 4)" && tar -zxvf setup.tar.gz
 chmod u+x ./*.sh && ./setup.sh
-mv -t ~ orca.conf update.sh
+mv -t ~ orca.conf update.sh utils.sh
 cd - && rm -Rf "$tmp"
