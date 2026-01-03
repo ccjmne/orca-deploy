8be88d7c713644071db2766d24ca66e0a7d48ae6
Éric NICOLAS <ccjmne@gmail.com>
2026-01-03 18:18:31 +0100

    Actually use the html2pdf.conf as docker env-file

--- ec2/setup/update.sh
+++ ec2/setup/update.sh
@@ -34,6 +34,7 @@ docker run -it -d           \
   --restart  unless-stopped \
   --publish  3000:3000      \
   --memory   512M           \
+  --env-file html2pdf.conf  \
   ghcr.io/ccjmne/puppeteer-html2pdf:latest
 
 docker stop orca || true && (docker rm orca || true)
--- ec2/utils/deploy.sh
+++ ec2/utils/deploy.sh
@@ -6,5 +6,5 @@ cd "$tmp"
 wget "$(curl -s https://api.github.com/repos/ccjmne/orca-deploy/releases | grep browser_download_url | grep setup.tar.gz | head -n 1 | cut -d '"' -f 4)"
 tar -zxvf setup.tar.gz
 ./setup.sh
-mv -t ~ orca.conf update.sh utils.sh
+mv -t ~ orca.conf html2pdf.conf update.sh utils.sh
 cd - && rm -Rf "$tmp"
